d2b1da02c80242a3881b5bf1f50bdd4224d4771c
Your Name <artpocket77@gmail.com>
2020-08-19 20:18:42 +0300

    function for determining the number of letters

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-let buttonClick = () => {
-    alert("Hello")
-    console.log("Hello")
+const firstRow = 'мама мыла раму';
+const secondRow = 'собака друг человека';
+let string = ''
+let toString = ''
+
+for (let i = 0; i <= firstRow.length; i++) {
+    if (firstRow.charAt(i) === 'а') {
+        string += i
+    }
 }
+
+for (let i = 0; i <= secondRow.length; i++) {
+    if (secondRow.charAt(i) === 'а') {
+        toString += i
+    }
+}
+function getRow() {
+    string.length > toString.length ? console.log(firstRow):console.log(secondRow)
+}
+getRow()
