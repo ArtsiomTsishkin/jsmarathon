7c5e7f7e619cfa951c567a77c3be87a920a8a197
Your Name <artpocket77@gmail.com>
2020-08-22 10:35:05 +0300

    function for determining the number of letters

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 const firstRow = 'мама мыла раму';
 const secondRow = 'собака друг человека';
-let string = ''
-let toString = ''
 
-for (let i = 0; i <= firstRow.length; i++) {
-    if (firstRow.charAt(i) === 'а') {
-        string += i
+
+const liter = 'a'
+
+
+getRow =(firstRow,secondRow ) => {
+    let string = 0;
+    let toString = 0;
+    for (let i = 0; i < firstRow.length; i++) {
+        if (firstRow.charAt(i) === 'а') {
+            string++
+        }
     }
-}
 
-
-for (let i = 0; i <= secondRow.length; i++) {
-    if (secondRow.charAt(i) === 'а') {
-        toString += i
+    for (let i = 0; i < secondRow.length; i++) {
+        if (secondRow.charAt(i) === 'а') {
+            toString++
+        }
     }
+
+   return  string>toString?console.log(firstRow):console.log(secondRow)
 }
-function getRow() {
-    string.length > toString.length ? console.log(firstRow):console.log(secondRow)
-}
-getRow()
+
+
+    getRow(firstRow, secondRow)
+
